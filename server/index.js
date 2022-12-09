929d88493cfe6d3c003cb1d884bfcad1b3e92318
mostafab <mostafa0104@gmail.com>
2022-12-09 16:45:53 -0500

    Remove unused files

--- server/index.js
+++ server/index.js
@@ -7,9 +7,6 @@ import httpsEnforce from 'express-sslify';
 
 import './config';
 
-import statsApiRouter from './api/stats-router';
-import tournamentApiRouter from './api/tournament-router';
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const ONE_WEEK_MS = 1000 * 60 * 60 * 24 * 7; 
@@ -28,17 +25,13 @@ const cacheTime = process.env.NODE_ENV === 'production' ? ONE_WEEK_MS : 0;
 app.use('/neg5.stats.web-server/', express.static(path.resolve(__dirname, '../react-ui/build'), { maxAge: cacheTime }));
 
 // Enable CORS
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-// Answer API requests.
-app.use('/api/t/:tournamentId/stats', statsApiRouter);
-app.use('/api/t', tournamentApiRouter);
-
-// Proxy requests to /neg5-api to the Spark Neg5 service
+// Proxy requests to /neg5-api to the backend API
 app.use('/neg5-api', httpProxy(process.env.NEG5_API_HOST, {
   proxyReqPathResolver: req => `/neg5-api${req.url}`
 }));

d5bf702e9110c6f9efc020a8d42e223db716b8c5
mostafab <mostafa0104@gmail.com>
2022-12-10 15:21:34 -0500

    Remove dependency on axios library

--- react-ui/src/client/tournament-client.js
+++ react-ui/src/client/tournament-client.js
@@ -1,57 +1,29 @@
-import axios from 'axios';
 import moment from 'moment'
 
-const INFO_URL = `/neg5-api/tournaments/{tournamentId}`;
-const RECENT_TOURNAMENTS_URL = `/neg5-api/search/tournaments/days?days={days}`;
-const BETWEEN_DATES_TOURNAMENT_URL = `/neg5-api/search/tournaments/dates`;
-const BY_NAME_URL = `/neg5-api/search/tournaments/name`;
-
 export const getTournamentInfo = async (tournamentId) => {
-  try {
-    const url = INFO_URL.replace('{tournamentId}', tournamentId);
-    return (await axios.get(url)).data;
-  } catch (error) {
-    throw error;
-  }
+  const url = `/neg5-api/tournaments/${tournamentId}`;
+  const response = await fetch(url);
+  return await response.json();
 }
 
 export const getRecentTournaments = async (daysSince) => {
-  try {
-    const url = RECENT_TOURNAMENTS_URL.replace('{days}', daysSince);
-    const { data } = await axios.get(url);
-    return data.result;
-  } catch (error) {
-    throw error;
-  }
+  const url = `/neg5-api/search/tournaments/days?days=${daysSince}`;
+  const response = await fetch(url);
+  return await response.json();
 }
 
 export const getTournamentsInRange = async (startDate, endDate) => {
-  try {
-    const url = BETWEEN_DATES_TOURNAMENT_URL;
-    const { data } = await axios.get(url, {
-      params: {
-        start: moment(startDate).format('YYYY-MM-DD'),
-        end: moment(endDate).format('YYYY-MM-DD'),
-      },
-    });
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const start = moment(startDate).format('YYYY-MM-DD');
+  const end = moment(endDate).format('YYYY-MM-DD');
+  const url = `/neg5-api/search/tournaments/dates?start=${start}&end=${end}`;
+  const response = await fetch(url);
+  return await response.json();
 }
 
 export const getTournamentsByName = async (searchQuery) => {
-  try {
-    const url = BY_NAME_URL;
-    const { data } = await axios.get(url, {
-      params: {
-        name: searchQuery,
-      },
-    })
-    return data;
-  } catch (error) {
-    throw error;
-  }
+  const url = `/neg5-api/search/tournaments/name?name=${searchQuery}`;
+  const response = await fetch(url);
+  return await response.json();
 }
 
 export default {

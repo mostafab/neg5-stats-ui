2ecc17437145d587713ea271c2c14305501090a8
mostafab <mostafa0104@gmail.com>
2022-12-09 23:44:37 -0500

    Adding nvmrc file

--- bin/setup_environment.sh
+++ bin/setup_environment.sh
@@ -3,21 +3,18 @@
 set -e
 
 nvm_script_location='https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh'
-node_version="8.17.0"
 
 cd $(dirname "$0")
+cd ../
+node_version=$(cat .nvmrc)
+
 curl -o- $nvm_script_location | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install "v${node_version}"
-cd ../
-nvm use $node_version
-cd react-ui
-nvm use $node_version
 
-cd ../
 yarn install
 cd react-ui
 yarn install

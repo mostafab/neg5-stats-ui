133203b22fa81400ad6fdef5867a6ff7486e6cfd
mostafab <mostafa0104@gmail.com>
2022-12-13 12:32:03 -0500

    Downgrade axios: https://github.com/axios/axios/issues/5346, use compression and helmet

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 // next.config.js
 
+// https://nextjs.org/docs/advanced-features/security-headers
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval';
+  child-src 'self';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  font-src 'self' https://fonts.gstatic.com;  
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 module.exports = {
   async redirects() {
     return [
@@ -12,6 +28,15 @@ module.exports = {
         source: "/t/:tournamentId/:slug",
         destination: "/t/:tournamentId/:slug/team-standings",
         permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
       },
     ];
   },
